c0468828d43948434e21c8cf15c8eecee2bdc0f9
Shiva26-k <shivakumar76712@gmail.com>
2025-07-25 13:11:12 +0530

    Update firstPipeline.groovy

--- vars/firstPipeline.groovy
+++ vars/firstPipeline.groovy
@@ -7,48 +7,48 @@ def call(Map pipelineParams){
     //instance of calculaotr
     Calculator calculator = new Calculator(this)
     pipeline{
-    agent{
-        label 'java-slave'
-    }
-    environment{
-        APPLICATION_NAME = "${pipelineParams.appName}"
-    }
-    stages{
-        stage('calculator'){
-            steps{
-                script{
-                    echo "calling add method"
-                    echo  "printinf the sum of values"
-                    println calculator.add(4,5)
+        agent{
+            label 'java-slave'
+        }
+        environment{
+            APPLICATION_NAME = "${pipelineParams.appName}"
+        }
+        stages{
+            stage('calculator'){
+                steps{
+                    script{
+                        echo "calling add method"
+                        echo  "printinf the sum of values"
+                        println calculator.add(4,5)
+                    }
+                }
+            }
+            stage('Build'){
+                steps{
+                    echo "*****build the application****"
+                    echo "****I am Building for ${env.APPLICATION_NAME}"
+                }
+            }
+            stage('Test'){ 
+                steps{
+                    echo "********Application testing********"
+                }
+            }
+            stage('DeploytoDev'){
+                steps{
+                    echo "Deploying to dev environment"
+                }
+            }
+            stage('Deploytostage'){
+                steps{
+                    echo "Deploy to stage environment"
+                }
+            }
+            stage('DeploytoProd'){
+                steps{
+                    echo "Deploy to prod environment"
                 }
             }
         }
-        stage('Build'){
-            steps{
-                echo "*****build the application****"
-                echo "****I am Building for ${env.APPLICATION_NAME}"
-            }
-        }
-        stage('Test'){ 
-            steps{
-                echo "********Application testing********"
-            }
-        }
-        stage('DeploytoDev'){
-            steps{
-                echo "Deploying to dev environment"
-            }
-        }
-        stage('Deploytostage'){
-            steps{
-                echo "Deploy to stage environment"
-            }
-        }
-        stage('DeploytoProd'){
-            steps{
-                echo "Deploy to prod environment"
-            }
-        }
     }
-}
 }
